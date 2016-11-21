00ac85bbf97ae9b42fc307b67c9f9f96be5cd87e
StompyNZ <russell@codestorm.nz>
2016-11-22 10:54:12 +1300

    Add project files.

---
+++ SignalRDemo/HitCounterHub.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Microsoft.AspNet.SignalR;
+using Microsoft.AspNet.SignalR.Hubs;
+
+namespace SignalRDemo
+{
+    [HubName("hitCounterHub")]
+    public class HitCounterHub : Hub
+    {
+        static int _counter = 0;
+        public void RecordHit()
+        {
+            _counter += 1;
+            Clients.All.updateHits(_counter);
+        }
+    }
+}
---
+++ SignalRDemo/Scripts/site.js
@@ -0,0 +1,26 @@
+﻿/// <reference path="jquery-1.6.4.js" />
+/// <reference path="jquery.signalR-2.2.1.js" />
+/// <reference path="knockout-3.4.0.js" />
+
+function AppViewModel() {
+    self = this;
+    self.firstName = ko.observable("Russell");
+    self.lastName = ko.observable("Thompson");
+    self.counter = ko.observable("0");
+
+    self.update = function (hitCount) {
+        self.counter(hitCount);
+    };
+}
+
+var appViewModel = new AppViewModel();
+ko.applyBindings(appViewModel);
+
+var counterHub = null;
+$(function () {
+    counterHub = $.connection.hitCounterHub;
+    counterHub.client.updateHits = appViewModel.update;
+    $.connection.hub.start().done(function () {
+        counterHub.server.recordHit();
+    });
+});
